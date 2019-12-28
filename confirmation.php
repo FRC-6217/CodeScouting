fc56b0054ecaa7f570db0c7eeb657685d565f6d8
Joe Coyle <jtcoyle@startech-comp.com>
2019-12-28 16:01:56 -0600

    Add files via upload

--- confirmation.php
+++ confirmation.php
@@ -30,7 +30,7 @@
 
     $insertingData = "INSERT into scoutrecord (matchId, robotId, scoutId, leaveHAB, ssHatchCnt, ssCargoCnt, toHatchCnt, toCargoCnt, playedDefense, returnToHAB) values ($match, $robot, $scouts, '$lev', $ssHatchCnt, $ssCargoCnt, $toHatchCnt, $toCargoCnt, '$def', '$ret');";
 
-    $tsql = "execute sp_ins_scoutRecord $scout, $match, $team, $leaveHab, $ssHatchCnt, $ssCargoCnt, @toHatchCnt, @toCargoCnt, $defense, $returnHab";
+    $tsql = "execute sp_ins_scoutRecord $scout, $match, $team, $leaveHab, $ssHatchCnt, $ssCargoCnt, $toHatchCnt, $toCargoCnt, $defense, $returnHab";
 	$results = sqlsrv_query($conn, $tsql);
 	if($executing) {
 		echo "Submittion Succeeded!";
