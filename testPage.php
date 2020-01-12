ac378e9451c04e11130f958ae8e801e05f672399
Joe Coyle <jtcoyle@startech-comp.com>
2020-01-11 23:32:10 -0600

    Add files via upload

--- testPage.php
+++ testPage.php
@@ -68,12 +68,12 @@
 <br>
 <?php
 	echo "Test Writing";
-    $response = http_get("https://www.thebluealliance.com/api/v3/event/2020mndu/teams"
+    $response = http_get('https://www.thebluealliance.com/api/v3/event/2020mndu/teams'
 	                   , array('headers' => array('X-TBA-Auth-Key'=>'N4Z1bSR1oaDFECjDNV3wp1zAqUY0LCI4OZyL1nVCg2K5yfsV3JAy9OBuJgEKYQ7M')), $info);
-	echo($response);
-	print_r($info);
 	print_r($response);
+	print_r($info);
 	echo($info);
+	echo($response);
 	var_dump(json_decode($info));
 	var_dump(json_decode($info, true));
 ?>
