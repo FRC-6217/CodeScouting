79835a74604a0cbb652c714112afe2e2c8773ab0
Joe Coyle <jtcoyle@startech-comp.com>
2020-01-14 11:26:56 -0600

    Add files via upload

---
+++ eventSetup.php
@@ -0,0 +1,106 @@
+<html>
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>Scouting App</title>
+     <link rel="stylesheet" type="text/css" href="Style/scoutingStyle.css">
+	<head>
+		<link rel="apple-touch-icon" sizes="57x57" href="/Logo/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/Logo/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/Logo/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/Logo/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/Logo/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/Logo/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/Logo/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/Logo/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/Logo/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="/Logo/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/Logo/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/Logo/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/Logo/favicon-16x16.png">
+        <link rel="manifest" href="/Logo/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/Logo/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<body>
+			<h1><center>Bomb Botz Scouting App</center></h1>
+		</body>
+		<center><a href="index.php">Home</a></center>
+<?php
+	// Initial setup of Database Connection
+	ini_set('display_errors', '1');
+    $serverName = getenv("ScoutAppDatabaseServerName");
+	$database = getenv("Database");
+	$userName = getenv("DatabaseUserName");
+	$password = getenv("DatabasePassword");
+    $connectionOptions = array(
+        "Database" => "$database",
+        "Uid" => "$userName",
+        "PWD" => "$password"
+    );
+    // Establishes the DB connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+
+	// Setup Blue Alliance API calls
+	$TBAAuthKey = getenv("TheBlueAllianceAuthKey");
+	$aHTTP['http']['method']  = "GET";
+	$aHTTP['http']['header']  = "X-TBA-Auth-Key: " . $TBAAuthKey. "\r\n";
+	$aHTTP['http']['header'] .= "Accept: application/json\r\n";
+	$context = stream_context_create($aHTTP);
+?>
+			<center>				
+				<div class="container" id="event">
+					<p><u><b>Event Selection</b></u></p>
+					<p>Game:
+						<select style="width: 161px;" name="game">
+							<option value=""></option>
+							<?php
+							$tsql = "select id, name, gameYear, isActive from game order by gameYear desc";
+							$getResults = sqlsrv_query($conn, $tsql);
+							if ($getResults == FALSE)
+								if( ($errors = sqlsrv_errors() ) != null) {
+									foreach( $errors as $error ) {
+										echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+										echo "code: ".$error[ 'code']."<br />";
+										echo "message: ".$error[ 'message']."<br />";
+									}
+								}
+							while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+							?>
+							<?php if ($row['isActive'] = "Y")
+							         echo '<option value="$row['id']" selected>$row['name']</option>';
+								  else
+							         echo '<option value="$row['id']">$row['name']</option>';
+							<?php		
+							}
+							?>
+						</select>
+						</p>
+					<p></p>
+					
+					<p>Event:
+						<select style="width: 157px" name="event">
+							<option value="<?php echo ($matchId);?>" selected><?php echo ($matchNumber);?></option>
+							<?php
+							$tsql = "select m.matchId, m.matchNumber from v_MatchHyperlinks m order by m.sortOrder, m.matchNumber";
+							$getResults = sqlsrv_query($conn, $tsql);
+							if ($getResults == FALSE)
+								if( ($errors = sqlsrv_errors() ) != null) {
+									foreach( $errors as $error ) {
+										echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+										echo "code: ".$error[ 'code']."<br />";
+										echo "message: ".$error[ 'message']."<br />";
+									}
+								}
+							while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+							?>
+							<option value="<?php echo ($row['matchId']);?>"><?php echo ($row['matchNumber']);?></option>
+							<?php		
+							}
+							?>
+						</select>
+					</p>
+					<p></p>
+				</div>
+            </center>
+</html> 
