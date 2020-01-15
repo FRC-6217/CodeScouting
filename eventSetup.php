555e995c04f98e39e85e51870aabd5fd26f1eb25
Joe Coyle <jtcoyle@startech-comp.com>
2020-01-14 23:58:52 -0600

    Add files via upload

--- eventSetup.php
+++ eventSetup.php
@@ -94,10 +94,12 @@
 										echo "message: ".$error[ 'message']."<br />";
 									}
 								}
-							if (empty($row))
-								echo '<option value="" selected></option>';
-							else
-								$eventCode = $row["event_code"];
+							while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+								if (empty($row))
+									echo '<option value="" selected></option>';
+								else
+									$eventCode = $row["event_code"];
+							}
 							sqlsrv_free_stmt($getResults);
 
 							// Events from Blue Alliance
