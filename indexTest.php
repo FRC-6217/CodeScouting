b7f4313d086a3c5213400b425fb4ec0761a6308e
Joe Coyle <jtcoyle@startech-comp.com>
2020-02-03 11:11:39 -0600

    Add files via upload

--- indexTest.php
+++ indexTest.php
@@ -104,8 +104,8 @@
     <br>
 	<center><div class="g-signin2" data-onsuccess="onSignIn"></div></center>
 	<br>
-	<button id="verify_in_server">verify in server</button>
-	<div id="verify_in_server_result"></div>
+	<center><button id="verify_in_server">verify in server</button>
+	<div id="verify_in_server_result"></div></center>
 	<br>
 	<center><table cellspacing="0" cellpadding="5">
     <tr>
