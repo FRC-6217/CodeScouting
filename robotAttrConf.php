a2d59c2d047032b3f2bfdd87f1e36e90f35fa970
Joseph T. Coyle <jcoyle@stc.local>
2020-02-29 23:07:36 -0600

    Scout Robot

--- robotAttrConf.php
+++ robotAttrConf.php
@@ -41,7 +41,7 @@
 	</h2>
 	<p></p>
 <?php
-    $tsql = "sp_ins_scoutRobot $teamId";
+    $tsql = "sp_ins_scoutRobot $value1";
 	echo "Hello World" . $tql;
 	if (isset($value2))
 		$tsql .= ", $value2";
