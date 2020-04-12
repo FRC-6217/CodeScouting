1ff5a95a458f4613965728692e6d1c4d423309ff
Joseph T. Coyle <jcoyle@stc.local>
2020-04-12 11:06:19 -0500

    Add Video Links to App

--- Reports/matchReport.php
+++ Reports/matchReport.php
@@ -513,8 +513,6 @@
 					echo "message: ".$error[ 'message']."<br />";
 				}
 			}
-		echo '<iframe width="560" height="315" src="https://www.thebluealliance.com/match/2020mndu_qm9" frameborder="0" allowfullscreen></iframe>';
-		echo '<p></p>';
 		while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
         	echo '<iframe width="560" height="315" src="' . $row['videoUrl'] . '" frameborder="0" allowfullscreen></iframe>';
 			echo '<p></p>';
