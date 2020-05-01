87a7a42b4b6951eb120eed939d2a8f1c65b1dcf8
Joseph T. Coyle <jcoyle@stc.local>
2020-05-01 11:37:29 -0500

    Add Scout Comment to all scout records

--- confirmation.php
+++ confirmation.php
@@ -80,7 +80,7 @@
 	 <p></p>
 	 <center><a class="clickme danger" href="index.php">Home</a></center>
 <?php
-    $tsql = "sp_ins_scoutRecord $scout, $match, $team, $alliancePosition, $scoutComment, '$value1'";
+    $tsql = "sp_ins_scoutRecord $scout, $match, $team, $alliancePosition, '$scoutComment', '$value1'";
 	if (isset($value2))
 		$tsql .= ", '$value2'";
 	if (isset($value3))
