dbb3740bd260c01cff6b191a2dc3d30ff9b8b667
josephtcoyle <jtcoyle@startech-comp.com>
2023-01-02 13:08:55 -0600

    Troubleshoot

--- robotAttrConf6217.php
+++ robotAttrConf6217.php
@@ -21,10 +21,10 @@
 
     // Get posted variables
 	//$submit = $POST[submitToDatabase];
-	//$teamId = $_POST[teamId];
+	$teamId = $_POST[teamId];
 	$loginEmailAddress = getenv("DefaultLoginEmailAddress");
 	$tsql = "select scoutGUID from Scout where emailAddress = '$loginEmailAddress'";
-	//echo "Team: ".$teamId."<br />";
+	echo "Team: ".$teamId."<br />";
 	echo "Email: ".$loginEmailAddress."<br />";
 	echo "SQL: ".$tsql."<br />";
     $getResults = sqlsrv_query($conn, $tsql);
