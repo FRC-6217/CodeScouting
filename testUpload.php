d45c0283c4e8f967af02c5ec80204e7c1740f715
FRC 6217 <frc6217@gmail.com>
2024-12-08 12:01:05 -0600

    Cleanup unused code

--- testUpload.php
+++ testUpload.php
@@ -63,74 +63,6 @@ if(isset($_POST["submit"])) {
         
         echo "Calling Function storageAddFile.<p></p>";
         storageAddFile($containerName, $tmp_file, $fileNameOnStorage, $mime, $storageAccountName, $accessKey);
-
-/*
-$connectionString = "DefaultEndpointsProtocol=http;AccountName=$storageAccountName;AccountKey=$accessKey";
-
-// Create blob REST proxy.
-$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
-
-
-try {
-    // List blobs.
-    $blob_list = $blobRestProxy->listBlobs(<container>);
-    $blobs = $blob_list->getBlobs();
-
-    echo "Display list of Blobs<br />";
-    foreach($blobs as $blob)
-    {
-        echo $blob->getName().": ".$blob->getUrl()."<br />";
-    }
-}
-catch(ServiceException $e){
-    // Handle exception based on error codes and messages.
-    // Error codes and messages are here: 
-    // http://msdn.microsoft.com/en-us/library/windowsazure/dd179439.aspx
-    $code = $e->getCode();
-    $error_message = $e->getMessage();
-    echo $code.": ".$error_message."<br />";
-}
-*/
-/*
-        $mimeType = $check["mime"];
-        $blobName = 'folder/'. hash_name("sha256", $file);
-        $dateTime = gmdate('D, d M Y H:i:s \G\M\T');
-        $headerResource = "x-ms-blob-cache-control:max-age=3600\nx-ms-blob-type:BlockBlob\nx-ms-date:$dateTime\nx-ms-version:2019-12-12";
-        echo "File Name: $target_file, Blob Name: $blobName.<p></p>";
-
-        // Generate signature
-        $arraysign = [];  // initiate an empty array (don't remove this 🙂)
-        $arraysign[] = 'PUT';               //HTTP Verb
-        $arraysign[] = '';                  //Content-Encoding
-        $arraysign[] = '';                  //Content-Language
-        $arraysign[] = $fileSize;           //Content-Length (include value when zero)
-        $arraysign[] = '';                  //Content-MD5
-        $arraysign[] = $mimeType;           //Content-Type
-        $arraysign[] = '';                  //Date
-        $arraysign[] = '';                  //If-Modified-Since
-        $arraysign[] = '';                  //If-Match
-        $arraysign[] = '';                  //If-None-Match
-        $arraysign[] = '';                  //If-Unmodified-Since
-        $arraysign[] = '';                  //Range
-        $arraysign[] = $headerResource;     //CanonicalizedHeaders
-        $arraysign[] = $urlResource;        //CanonicalizedResource
-
-        // Converts the array to a string as required by MS 
-        $str2sign = implode("\n", $arraysign);
-        $sig = base64_encode(hash_hmac('sha256', utf8_encode($str2sign), base64_decode($accessKey), true));
-        $url = "https://$storageAccountName.blob.core.windows.net/$containerName/{$blobName}";
-
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, [
-          'fileToUpload' => new CURLFile($file)
-        ]);
-        
-        $response = curl_exec($curl);
-        curl_close($curl);
-        
-        echo $response;
-*/
     }
 }
 
@@ -155,8 +87,6 @@ function storageAddFile($containerName, $file, $fileName, $mime, $storageAccount
 
         # Identify MIME type
         try {
-//            $mimes = new \Mimey\MimeTypes;
-//            $mime = $mimes->getMimeType(pathinfo($fileName, PATHINFO_EXTENSION));
             $options->setContentType($mime);
         } catch (Exception $e) {
             echo "Failed to read MIME from '" . $file . "': " . $e . "<p></p>";
@@ -170,7 +100,7 @@ function storageAddFile($containerName, $file, $fileName, $mime, $storageAccount
                     $options->setCacheControl("public, max-age=" . $cacheTime);
                 }
             }
-            $blobClient->createBlockBlob($containerName, $fileName, $handle, $options);
+            $blobClient->createBlockBlob($containerName, "1234/" . $fileName, $handle, $options);
         } catch (Exception $e) {
             echo "Failed to upload file '" . $file . "' to storage: " . $e . "<p></p>";
         }
@@ -186,55 +116,5 @@ function storageAddFile($containerName, $file, $fileName, $mime, $storageAccount
     }
 }
 
-# Get cache time by MIME type
-function getCacheTimeByMimeType($mime) {
-    $mime = strtolower($mime);
-    $types = array(
-        "application/json" => 604800, // 7 days
-        // Add more MIME types and cache times as needed
-    );
-    return $types[$mime] ?? null;
-}
-
-function uploadToAzureCloud($file)
-{
-    try { 
-        $orignalFileName = $file->getClientOriginalName();
-
-        // use GuzzleHttp\Client;
-        $client = new Client();
-        $response = $client->request('PUT', $url, [
-            'headers' => [
-            'Authorization' => "SharedKey $storageAccountName:$sig",
-            'x-ms-blob-cache-control' => 'max-age=3600',
-            'x-ms-blob-type' => 'BlockBlob',
-            'x-ms-date' => $dateTime,
-            'x-ms-version' => '2019-12-12',
-            'Content-Type' => $mimeType,
-            'Content-Length' => $fileSize
-            ],
-            'body' => fopen($file->path(), 'r'),
-        ]);          
-        if ($response->getStatusCode() == 201) {
-            // image sas token 
-//          $urlSasToken = config('services.azure_storage.sas_token');
-            return
-                [
-                'original_name' =>   $orignalFileName,
-//             'media_url' => "$url?$urlSasToken",
-                'media_url' => "$url"
-                ];
-        } else {
-            echo "Something went wrong, Media URL: $url.<p></p>";
-            return response()->json(['message' => 'Something went wrong']);
-        }
-    }
-    catch (RequestException $e) {
-        // If there's an error, log the error message
-        $errorMessage = $e->getMessage();
-        echo "Caught Exception: $errorMessage.<p></p>";
-        return response()->json(['error' => $errorMessage], 500);
-    }
-}
 ?>
 </html>
