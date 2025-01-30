f9a153b20f3df8ef3b2281c421e5cbc0decb93c0
FRC 6217 <frc6217@gmail.com>
2025-01-30 17:06:04 -0600

    Add RP and Coop data to the Rank table

--- Reports/rankReport.php
+++ Reports/rankReport.php
@@ -98,10 +98,41 @@
 				$cnt = $cnt + 1;
 			}
 			sqlsrv_free_stmt($getResults);
-			echo "<th><a href='../Reports/rankReport.php?sortOrder=eventRank&rankName=Ranking Points'>Event<br/>Rank</a></th>";
 			?>
+			<th><a href='../Reports/rankReport.php?sortOrder=eventRank&rankName=Ranking Points'>Event<br/>Rank</a></th>
             <th>Rank<br/>Pts</th>
             <th>OPR</th>
+			<?php
+			// Display table headers for the Ranking Points/Cooperition realized
+			$tsql = "select grp.tableHeader
+   						  , grp.sortOrder
+		 			   from GameRankingPoint grp
+			  				inner join v_GameEvent ge
+			  				on ge.gameId = grp.gameId
+					  where ge.loginGUID = '$loginGUID'
+				     union
+	   				 select 'Coop' tableHeader
+						  , 99 sortOrder
+					   from Game g
+			  				inner join v_GameEvent ge
+			  				on ge.gameId = g.id
+					  where ge.loginGUID = '$loginGUID'
+		  				and g.tbaCoopMet is not null
+				     order by sortOrder";
+			$getResults = sqlsrv_query($conn, $tsql);
+			if ($getResults == FALSE)
+				if( ($errors = sqlsrv_errors() ) != null) {
+					foreach( $errors as $error ) {
+						echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+						echo "code: ".$error[ 'code']."<br />";
+						echo "message: ".$error[ 'message']."<br />";
+					}
+				}
+			while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+				echo "<th>" . $row['tableHeader'] . "</th>";
+			}
+			sqlsrv_free_stmt($getResults);
+			?>
         </tr>
 		</thead>
 		<tbody>
@@ -169,6 +200,10 @@ $tsql = "execute sp_rpt_rankReport '$sortOrder', '$loginGUID'";
 			echo "<td>" . $row['eventRank'] . "</td>";
 			echo "<td>" . $row['rankingPointAverage'] . "</td>";
 			echo "<td>" . $row['oPR'] . "</td>";
+			if (isset($row['rp1TableHeader'])) echo "<td>" . $row['rp1Total'] . "</td>";
+			if (isset($row['rp2TableHeader'])) echo "<td>" . $row['rp2Total'] . "</td>";
+			if (isset($row['rp3TableHeader'])) echo "<td>" . $row['rp3Total'] . "</td>";
+			if (isset($row['coopTableHeader'])) echo "<td>" . $row['coopTotal'] . "</td>";
 			echo "</tr>";
 		}
 	}
